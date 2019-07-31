b6af007f103980951e2b77d60f8c5e827b406cf2
tanisha2398 <tanishanegipro@gmail.com>
2019-07-31 22:52:10 +0530

    setup heroku env variable

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,20 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  // apiKey: process.env.FIREBASE_API_KEY,
-  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.FIREBASE_DATABASE_URL,
-  // projectId: process.env.FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.FIREBASE_APP_ID
-  apiKey: "AIzaSyCle7HY74Z-TX5yDkuXX7GxRVYLoo0PPyY",
-  authDomain: "blogapp-aa416.firebaseapp.com",
-  databaseURL: "https://blogapp-aa416.firebaseio.com",
-  projectId: "blogapp-aa416",
-  storageBucket: "blogapp-aa416.appspot.com",
-  messagingSenderId: "155110135697",
-  appId: "1:155110135697:web:9ee9db07285383fa"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+  // apiKey: "AIzaSyCle7HY74Z-TX5yDkuXX7GxRVYLoo0PPyY",
+  // authDomain: "blogapp-aa416.firebaseapp.com",
+  // databaseURL: "https://blogapp-aa416.firebaseio.com",
+  // projectId: "blogapp-aa416",
+  // storageBucket: "blogapp-aa416.appspot.com",
+  // messagingSenderId: "155110135697",
+  // appId: "1:155110135697:web:9ee9db07285383fa"
 };
 
 firebase.initializeApp(firebaseConfig);
